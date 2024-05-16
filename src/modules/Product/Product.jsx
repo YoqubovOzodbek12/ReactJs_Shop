2004e2b8206e0f995a7625a4e505a9bb7f310bce
Ozodbek <ozodbek@MBP-Ozodbek.mshome.net>
2024-05-16 12:57:55 +0500

    alert update

--- src/modules/Product/Product.jsx
+++ src/modules/Product/Product.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Product = () => {
 
@@ -37,9 +39,9 @@ const Product = () => {
     } else {
       localStorage.setItem('cart', JSON.stringify([...cart, { ...product, quantity: 1 }]))
     }
-    alert('Product added to cart')
+    toast.success("Success !")
     console.log(redirect)
-    if(redirect) {
+    if(redirect)  {
       navigate('/cart')
     }
   }
@@ -126,6 +128,7 @@ const Product = () => {
                   Buy it now </button>
                 <button className="flex ml-auto border border-indigo-950 py-2 px-4 focus:outline-none hover:bg-indigo-600 hover:text-white rounded" onClick={() => handleCart(product)}>
                   Add to Card </button>
+                  <ToastContainer />
               </div>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
